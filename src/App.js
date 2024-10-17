b7287ed22f0080a74ec2060d02cb064941f19797
marcusrprojects <marcusr4231@gmail.com>
2024-10-17 12:22:19 -0400

    Updated to accomodate for change from Switch -> Routes in React.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import Categories from './components/Categories';
-import Profile from './components/Profile';
-import './App.css';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import './firebaseConfig';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Categories from "./components/Categories";
+import Profile from "./components/Profile";
+import "./App.css";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import "./firebaseConfig";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <div className="App-router">
-          <Switch>
+          <Routes>
             <Route exact path="/" component={Home} />
             <Route path="/categories" component={Categories} />
             <Route path="/profile" component={Profile} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
-          </Switch>
+          </Routes>
         </div>
       </Router>
     </div>
