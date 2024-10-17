138c90777a229253bf99c45b4d49ab48fbe85c6f
marcusrprojects <marcusr4231@gmail.com>
2024-10-16 21:33:15 -0400

    Multi-page app setup.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Categories from './components/Categories';
+import Profile from './components/Profile';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="App-router">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/categories" component={Categories} />
+            <Route path="/profile" component={Profile} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
